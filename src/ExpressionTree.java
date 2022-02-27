8d114190b02cfa2a12d844c8bf38b8e73c24c717
Andrew Dibble <92191287+adibble14@users.noreply.github.com>
2022-02-27 11:54:56 -0800

    Update ExpressionTree.java
    adding methods that this class needs to have

--- src/ExpressionTree.java
+++ src/ExpressionTree.java
@@ -1,2 +1,7 @@
 public class ExpressionTree {
+    private ExpressionTreeNode root;
+
+    public void makeEmpty(){}
+    public void printTree(){}
+    public int Evaluate(Spreadsheet spreadsheet){return 0;}
 }
