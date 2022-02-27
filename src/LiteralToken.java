9f24af37cb7df74c4d4e9109687babe8f153f81e
Andrew Dibble <92191287+adibble14@users.noreply.github.com>
2022-02-27 11:55:37 -0800

    Update LiteralToken.java
    adding a constructor and a method that needs to be added

--- src/LiteralToken.java
+++ src/LiteralToken.java
@@ -1,2 +1,8 @@
 public class LiteralToken extends Token {
+    private int value;
+
+    public LiteralToken(int value){this.value = value;};
+
+    public static String getValue(LiteralToken litToken){return "";}
+
 }
