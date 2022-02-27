69e81be57a3b169c985dc6f71b6b9fe0f1be7b35
Andrew Dibble <92191287+adibble14@users.noreply.github.com>
2022-02-27 11:58:21 -0800

    Update Stack.java
    added methods that the stack needs to implement

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,15 @@
 public class Stack {
+
+    public boolean isEmpty(){return false;}
+
+    public void pop(){}
+
+    public void push(Object obj){}
+
+    public Object topAndPop(){return null;}
+
+    public void makeEmpty(){}
+
+    public Object top(){return null;}
+
 }
