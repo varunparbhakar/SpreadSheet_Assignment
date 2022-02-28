da6141311c4290e3c6bc3dd64dfab3efe7491436
vaunparbhakar <varunparbhakar@yahoo.in>
2022-02-27 16:07:23 -0800

    Added Getters and Setter for ExpressionTreeNode

--- src/ExpressionTreeNode.java
+++ src/ExpressionTreeNode.java
@@ -2,4 +2,28 @@ public class ExpressionTreeNode {
     private Token token;
     ExpressionTreeNode left;
     ExpressionTreeNode right;
+
+    public Token getToken() {
+        return token;
+    }
+
+    public void setToken(Token token) {
+        this.token = token;
+    }
+
+    public ExpressionTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(ExpressionTreeNode left) {
+        this.left = left;
+    }
+
+    public ExpressionTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(ExpressionTreeNode right) {
+        this.right = right;
+    }
 }
