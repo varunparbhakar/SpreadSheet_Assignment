d8734988eef557302eeed05a8ad2f0821e19ec68
Andrew Dibble <92191287+adibble14@users.noreply.github.com>
2022-02-27 16:06:34 -0800

    Update ExpressionTreeNode.java
    added constructor

--- src/ExpressionTreeNode.java
+++ src/ExpressionTreeNode.java
@@ -2,4 +2,10 @@ public class ExpressionTreeNode {
     private Token token;
     ExpressionTreeNode left;
     ExpressionTreeNode right;
+    
+    public ExpressionTreeNode(Token token, ExpressionTreeNode left, ExpressionTreeNode right){
+        this.token = token;
+        this.left = left;
+        this.right = right;
+    }
 }
