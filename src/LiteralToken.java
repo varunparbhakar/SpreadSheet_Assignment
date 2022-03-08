e31bdabb009938fdae78619a95bde7d4fe6070f9
vaunparbhakar <varunparbhakar@yahoo.in>
2022-03-07 18:07:51 -0800

    LiteralToken.java fully commented

--- src/LiteralToken.java
+++ src/LiteralToken.java
@@ -1,14 +1,32 @@
+/**
+ * The LiteralToken class manages the literal object.
+ * @version 03/07/2022
+ * @author Varun Parbhakar, Andrew Dibble, and Minh Trung Le.
+ */
 public class LiteralToken extends Token {
-    private final int value;
+    private final int value; //int values of the LiteralToken
 
+    /**
+     * This is constructor for LiteralToken
+     * @param value
+     */
     public LiteralToken(int value) {
         this.value = value;
     }
 
+    /**
+     * This method returns value of the literal token.
+     * @param litToken
+     * @return
+     */
     public static int getValue(LiteralToken litToken) {
         return litToken.value;
     }
 
+    /**
+     * toString method for literal token.
+     * @return
+     */
     @Override
     public String toString() {
         return Integer.toString(value);
